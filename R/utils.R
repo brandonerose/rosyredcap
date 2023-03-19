fea686ae1f7548b1644b9934c7f28cc3fbdc1a9b
brandonerose <brandon.ed.rose@gmail.com>
2023-03-19 17:44:17 -0400

    changing write_xl to be more generic

--- R/utils.R
+++ R/utils.R
@@ -19,13 +19,14 @@ split_choices<-function(x){
   data.frame(code=x %>% purrr::map(1) %>% unlist(),name=x %>% purrr::map(2) %>% unlist())
 }
 
-write_xl<-function(DF,RC,path){
+write_xl<-function(DF,DB,path){
   wb <- openxlsx::createWorkbook()
   openxlsx::addWorksheet(wb, "sheet")
-  if(nrow(DF)>0){
-    DF$redcap_link<-paste0("https://redcap.miami.edu/redcap_v12.0.23/DataEntry/record_home.php?pid=",RC$PID,"&id=",DF[[RC$id_col]],"&arm=1")
+  COL<-which(colnames(DF)==DB$id_col)
+  if(nrow(DF)>0&&length(COL)>0){
+    DF$redcap_link<-paste0("https://redcap.miami.edu/redcap_v12.0.23/DataEntry/record_home.php?pid=",DB$PID,"&id=",DF[[DB$id_col]],"&arm=1")
     class(DF$redcap_link) <- "hyperlink"
-    openxlsx::writeData(wb, sheet = 1, x = DF$redcap_link,startRow = 2,startCol = which(colnames(DF)==RC$id_col))
+    openxlsx::writeData(wb, sheet = 1, x = DF$redcap_link,startRow = 2,startCol = COL)
     DF$redcap_link<-NULL
   }
   openxlsx::writeData(wb, sheet = 1, x = DF)
