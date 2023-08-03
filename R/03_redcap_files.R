ce38d7764e28ad0db88316bf3e6bb64cdc9373fb
brandonerose <brandon.ed.rose@gmail.com>
2023-08-03 18:10:29 -0400

    fix null

--- R/03_redcap_files.R
+++ R/03_redcap_files.R
@@ -1,38 +1,50 @@
-upload_file_to_redcap <- function(file,record, field,repeat_instance = character(0),event=character(0)){
+upload_file_to_redcap <- function(file,record, field,repeat_instance = NULL,event = NULL){
   # DB <- validate_DB(DB)
   file<-normalizePath(file)
   if(!file.exists(file)) stop("File does not exist! --> ",file)
+  body <- list(
+    "token"=validate_redcap_token(DB),
+    action='import',
+    content='file',
+    record =record,
+    field =field,
+    returnFormat='csv',
+    file=httr::upload_file(file)
+  )
+  if(!is.null(event)){
+    body$event <- event
+  }
+  if(!is.null(repeat_instance)){
+    body$repeat_instance <- repeat_instance
+  }
   response <- httr::POST(
     url = DB$redcap_uri,
-    body = list(
-      "token"=validate_redcap_token(DB),
-      action='import',
-      content='file',
-      record =record,
-      field =field,
-      event =event,
-      repeat_instance =repeat_instance,
-      returnFormat='csv',
-      file=httr::upload_file(file)
-    ),
+    body = body,
     encode = "multipart"
   )
   if(httr::http_error(response))stop("File upload failed")
-  message("File deleted! --> ",file)
+  message("File uploaded! --> ",file)
 }
 
-delete_file_from_redcap <- function(DB,record, field,repeat_instance = character(0), event = character(0)){
+delete_file_from_redcap <- function(DB,record, field,repeat_instance = NULL, event = NULL){
   # DB <- validate_DB(DB)
+  body <- list(
+    "token"=validate_redcap_token(DB),
+    action='delete',
+    content='file',
+    record =record,
+    field =field,
+    returnFormat='csv'
+  )
+  if(!is.null(event)){
+    body$event <- event
+  }
+  if(!is.null(repeat_instance)){
+    body$repeat_instance <- repeat_instance
+  }
   response <- httr::POST(
     url = DB$redcap_uri,
-    body = list(
-      "token"=validate_redcap_token(DB),
-      action='delete',
-      content='file',
-      record =record,
-      field =field,
-      returnFormat='csv'
-    )
+    body = body
   )
   if(httr::http_error(response))stop("File Delete failed")
   message("File Deleted!")
