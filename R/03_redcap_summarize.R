36dff4193de1a0618f979815392c0c8b34b4ad5c
brandonerose <brandon.ed.rose@gmail.com>
2024-01-05 09:35:54 -0500

    DEV

--- R/03_redcap_summarize.R
+++ R/03_redcap_summarize.R
@@ -28,10 +28,10 @@ summarize_DB <- function (DB,drop_dir=T){
   metadata <- metadata[which(!metadata$field_type=="checkbox_choice"),]
   # i <- 1:nrow(codebook) %>% sample(1)
   codebook$n <- 1:nrow(codebook) %>% lapply(function(i){
-      sum(DB$data[[codebook$form_name[i]]][,codebook$field_name[i]]==codebook$name[i],na.rm = T)
+    sum(merged[,codebook$field_name[i]]==codebook$name[i],na.rm = T)
   }) %>% unlist()
   codebook$n_total <- 1:nrow(codebook) %>% lapply(function(i){
-    sum(!is.na(DB$data[[codebook$form_name[i]]][,codebook$field_name[i]]),na.rm = T)
+    sum(!is.na(merged[,codebook$field_name[i]]),na.rm = T)
   }) %>% unlist()
   codebook$perc <-  codebook$n/codebook$n_total
   codebook$perc_text <- codebook$perc %>% magrittr::multiply_by(100) %>% round(1) %>% paste0("%")
