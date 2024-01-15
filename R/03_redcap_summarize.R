118a58cf0d0bae0e808d3a6deb580761af49ccd8
brandonerose <brandon.ed.rose@gmail.com>
2024-01-15 13:31:51 -0500

    dev

--- R/03_redcap_summarize.R
+++ R/03_redcap_summarize.R
@@ -4,6 +4,15 @@ annotate_codebook <- function (DB){
   #metadata/codebook =============
   metadata <- DB$metadata
   codebook <- DB$codebook %>% dplyr::select("field_name", "code", "name")
+
+  codebook <- unique(metadata$field_name) %>%
+    lapply(function(IN){
+      codebook[which(codebook$field_name==IN),]
+    }) %>% dplyr::bind_rows()
+
+  codebook<-codebook %>% merge(
+    metadata %>% dplyr::select(
+      "form_name","field_name","field_label","field_type","text_validation_type_or_show_slider_number"),by="field_name",sort=F)
   codebook$form_name <- 1:nrow(codebook) %>% lapply(function(i){
     form_name <- codebook$form_name[i]
     field_name <- codebook$field_name[i]
@@ -13,22 +22,14 @@ annotate_codebook <- function (DB){
         if(field_name%in%colnames(DB$data$merged))return("merged")
       }
       if("patient" %in% names(DB$data)){
-        if(field_name%in%colnames(DB$data$merged))return("patient")
+        if(field_name%in%colnames(DB$data$patient))return("patient")
       }
       for(other in names(DB$data)[which(!names(DB$data)%in%DB$instruments$instrument_name)]){
-        if(field_name%in%colnames(DB$data$other))return(other)
+        if(field_name%in%colnames(DB$data[[other]]))return(other)
       }
     }
     return(form_name)
   }) %>% unlist()
-  codebook <- unique(metadata$field_name) %>%
-    lapply(function(IN){
-      codebook[which(codebook$field_name==IN),]
-    }) %>% dplyr::bind_rows()
-
-  codebook<-codebook %>% merge(
-    metadata %>% dplyr::select(
-      "form_name","field_name","field_label","field_type","text_validation_type_or_show_slider_number"),by="field_name",sort=F)
 
   codebook$field_name_raw <- codebook$field_name
   codebook$field_name_raw[which(codebook$field_type=="checkbox_choice")]<-codebook$field_name[which(codebook$field_type=="checkbox_choice")] %>%
